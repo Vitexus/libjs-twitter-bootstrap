6e26989fe1420f54862fe00cbcdc23fb50edd66f
Vítězslav Dvořák <dvorak@austro-bohemia.cz>
2015-06-09 11:44:47 +0200

    Build number autoincrement

--- libjs-twitter-bootstrap.sh
+++ libjs-twitter-bootstrap.sh
@@ -13,12 +13,18 @@ npm install
 npm run build
 npm run dist
 
-BUILD=1
+if [ -e "../build" ]; then
+  BUILD=`cat  ../build | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
+else
+  BUILD="1";
+fi
+echo $BUILD > ../build
 VERSION=`cat _config.yml | grep current_version | awk '{ print $2 }'`
 #CHANGES=`cat CHANGELOG.md | awk -vRS="##" 'NR<=2' ORS="##" | grep -v "##"`
 #tar -czf ~/Projects/Packaging/libjs-twitter-bootstrap/libjs-twitter-bootstrap_$VERSION.orig.tar.gz .
-cd ~/Projects/Packaging/libjs-twitter-bootstrap
 #dch -v $VERSION-1 --package libjs-twitter-bootstrap $CHANGES
+cd ..
+
 dch -v $VERSION-$BUILD --package libjs-twitter-bootstrap 
 
 debuild -i -us -uc -b
